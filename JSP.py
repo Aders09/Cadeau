db8cba5c631f903929e85346f4143376d6475ad5
Aders09 <lahmeriadami@gmail.com>
2026-04-01 15:39:56 +0200

    Update JSP.py

--- JSP.py
+++ JSP.py
@@ -150,7 +150,7 @@ def falling_leaves():
 
 # --- PLAYLIST ---
 playlist = {
-    "The Smiths - Back to the old house": {"audio": "backto.mp3", "image": "backto.jpeg"},
+    "The Sms - Back to the old house": {"audio": "backto.mp3", "image": "backto.jpeg"},
     "ABBA - Dancing Queen": {"audio": "queen.mp3", "image": "queen.jpg"},
     "She & Him - I thought I saw your face today": {"audio": "ithought.mp3", "image": "ithought.jpeg"},
     "TV Girl - Better in the dark": {"audio": "dark.mp3", "image": "dark.jpeg"},
